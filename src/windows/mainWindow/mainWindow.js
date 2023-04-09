2e97da69f26dd69e66028785cbda007fd10bc091
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-04-09 17:55:24 +0200

    feat: add functionality to main window buttons

--- src/windows/mainWindow/mainWindow.js
+++ src/windows/mainWindow/mainWindow.js
@@ -1,1 +1,23 @@
- 
+ const {ipcRenderer} = require('electron');
+ const exitBtn = document.getElementById('exit');
+ const nextBtn = document.getElementById('next');
+ const algoTypes = document.querySelectorAll('input[name="main-question"]');
+
+ exitBtn.addEventListener('click', () => {
+        ipcRenderer.send('exit');
+ })
+
+ nextBtn.addEventListener('click', () => {
+    algoTypes.forEach(algoType => {
+        if (algoType.checked) {
+            ipcRenderer.send('gotoAlgorithmWindow', algoType.value);
+        }
+    });
+})
+
+algoTypes.forEach(type => {
+    type.addEventListener('change', () => {
+        console.log('algorithm selected');
+        nextBtn.disabled = false;
+    });
+})
