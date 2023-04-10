628627b9e317af3c993537b06cff709a48c69d45
Omar Fahmy <Ofahmy1234@gmail.com>
2023-04-11 01:57:58 +0200

    Feat: Add two Processses Class GUI and Input

--- src/util/test.js
+++ src/util/test.js
@@ -1,31 +1,37 @@
-const Algorithm = require('../util/algorithm');
+const Scheduler = require('./Scheduler');
+const { InputProcess } = require('./Process');
+
+const p1 = new InputProcess(1, 0, 5, 1);
+const p2 = new InputProcess(2, 1, 4, 2);
+const p3 = new InputProcess(3, 1, 3, 3);
 
 const processes = [
-    {
-      processId: 1,
-      arrivalTime: 3,
-      burstTime: 3,
-      priorityy: 3,
-    },
-    {
-      processId: 2,
-      arrivalTime: 1,
-      burstTime: 3,
-      priorityy: 2,
-    },
-    {
-      processId: 3,
-      arrivalTime: 2,
-      burstTime: 3,
-      priorityy: 1,
-    },
+    p1, 
+    p2,
+    p3,
   ];
 
-const alg = new Algorithm(processes);
+const alg = new Scheduler(processes);
 const tester = [];
 alg.on('draw', (segment) => {
     tester.push(segment);
     console.log(segment);
 }); 
+alg.on('append', (process) => {
+    alg.appendToQueue(process);
+})
 
 alg.nonPreemptivePriority();
+
+
+setTimeout(() => {
+    // alg.appendToQueue({
+    //     processId: 4,
+    //     arrivalTime: 3,
+    //     burstTime: 4,
+    //     priorityy: 1,
+    // });
+    alg.appendToQueue(new InputProcess(4, 3, 4, 1));
+
+}, 3000);
+
