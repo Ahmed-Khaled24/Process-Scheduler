dd0ef37ce60102a719d08ce5252f16d83d893ee6
Omar Fahmy <Ofahmy1234@gmail.com>
2023-04-11 22:21:02 +0200

    feat: Add the Live option for np-priority

--- src/util/Scheduler.js
+++ src/util/Scheduler.js
@@ -23,40 +23,51 @@ class Scheduler extends EventEmitter {
     this.count = 0;
   }
 
-  async nonPreemptivePriority() {
+  async nonPreemptivePriority(Live) {
+    // Helper flags
     let CPU = false;
     let currentProcess = null;
     let timeout = 0;
+    let emittedProcess =null;
+    let resultArr = [];
+
+    // Main Loop
     while (true) {
+      // Check if all processes are done
       if (this.inputProcesses.length == 0) {
         timeout++;
       }
+      // Break if timeout is reached
       if (timeout === 10) {
         break;
       }
+      // Filter processes that have arrived
       let filteredProcesses = this.inputProcesses.filter(
         (process) => process.arrivalTime <= this.count
       );
+      // Sort processes by priority
       filteredProcesses.sort((a, b) => a.priority - b.priority);
 
-      await promiseWait(1000);
+      // Wait for 1 second if live option selected
+      if(Live){
+        await promiseWait(1000);
+      }
 
+      // Check if there are any processes that have arrived
       if (filteredProcesses.length == 0) {
         continue;
+        // Check if CPU is free
       } else if (currentProcess === null) {
         currentProcess = {
           process: filteredProcesses[0],
           start: this.count,
           end: this.count + filteredProcesses[0].burstTime,
         };
-        // this.emit("draw", {
-        //   Pid: currentProcess.process.processId,
-        //   start: currentProcess.start,
-        //   end: currentProcess.end,
-        // });
-        this.emit("draw", new GUIProcess(currentProcess.process.processId, currentProcess.start, currentProcess.end));
+        emittedProcess = new GUIProcess(currentProcess.process.processId, currentProcess.start, currentProcess.end);
         CPU = true;
+
       } else {
+        // Check if current process is done
         if (this.count == currentProcess.end) {
           this.inputProcesses = this.inputProcesses.filter(
             (process) => process.processId !== currentProcess.process.processId
@@ -65,18 +76,17 @@ class Scheduler extends EventEmitter {
           CPU = false;
           this.count--;
         } else {
-          // this.emit("draw", {
-          //   Pid: currentProcess.process.processId,
-          //   start: currentProcess.start,
-          //   end: currentProcess.end,
-          // });
-          this.emit("draw", new GUIProcess(currentProcess.process.processId, currentProcess.start, currentProcess.end));
-
+          emittedProcess = new GUIProcess(currentProcess.process.processId, currentProcess.start, currentProcess.end);
         }
       }
-
+      if(Live){
+        this.emit("draw", emittedProcess);
+      }else{
+        resultArr.push(emittedProcess);
+      }
       this.count++;
     }
+    return resultArr;
   }
 
   appendToQueue(process) {
