7aa2a4a5426516657ff86628b7023bf601ec25bb
hanan saeed <hanansaeedbs12@gmail.com>
2023-04-12 03:43:36 +0200

    intial commit

--- src/main/algorithm.js
+++ src/main/algorithm.js
@@ -16,43 +16,75 @@ class Algorithm extends EventEmitter {
     this.count = 0;
   }
 
-  async PreemptiveSJF() {
-    while (this.inputProcesses.length) {
+  async PreemptiveSJF(live) {
+    let resultArr = [];
+    while (true) {
       let idx = 0;
+      let timeout = 0;
+      let emittedProcess = null;
+      let flag = false;
       this.inputProcesses.sort((a, b) => {
         if (a.burstTime != b.burstTime) return a.burstTime - b.burstTime;
         else return a.arrivalTime - b.arrivalTime;
       });
-      while (this.inputProcesses[idx].arrivalTime > this.count) {
-        idx++;
-        if (idx == this.inputProcesses.length) {
-          this.count++;
-          idx = 0;
+
+      while (this.inputProcesses.length == 0) {
+        if (live) await promiseWait(1000);
+        this.count++;
+        timeout++;
+        if (timeout == 10) {
+          flag = true;
+          break;
         }
       }
 
+      while (
+        this.inputProcesses.length != 0 &&
+        this.inputProcesses[idx].arrivalTime > this.count
+      ) {
+        idx++;
+        if (idx == this.inputProcesses.length) {
+          if (live) await promiseWait(1000);
+          this.count++;
+          timeout++;
+          if (timeout == 10) {
+            flag = true;
+            break;
+          }
+          idx = 0;
+        }
+      }
+      if (flag) break;
+
       this.inputProcesses[idx].burstTime -= 1;
 
-      let sentSegment = {
+      let currentProcess = {
         processId: this.inputProcesses[idx].processId,
         duration: {
           start: this.count,
           end: this.count + 1,
         },
       };
-      await promiseWait(1000);
-      this.emit(
-        "draw",
-        new GUIProcess(
-          sentSegment.processId,
-          sentSegment.start,
-          sentSegment.end
-        )
+
+      emittedProcess = new GUIProcess(
+        currentProcess.processId,
+        currentProcess.duration.start,
+        currentProcess.duration.end
       );
+      if (live) {
+        await promiseWait(1000);
+        this.emit("draw", emittedProcess);
+      } else {
+        resultArr = [...resultArr, emittedProcess];
+      }
+
       this.count += 1;
       if (this.inputProcesses[idx].burstTime == 0) {
         this.inputProcesses.splice(idx, 1);
       }
+    }
+    if (!live) {
+      this.emit("drawAll", resultArr);
     }
   }
   appendToQueue(process) {
