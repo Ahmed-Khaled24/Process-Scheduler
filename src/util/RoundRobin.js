bf290e577f05d2ad9187ede1340e4120d6de2cd9
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-04-14 13:59:37 +0200

    refactor: Round Robin class to connect with GUI

--- src/util/RoundRobin.js
+++ src/util/RoundRobin.js
@@ -1,6 +1,5 @@
-const { EventEmitter } = require("stream");
-const {InputProcess,GUIProcess,TimeCalculation} = require("./Process")
-
+const { EventEmitter } = require('stream');
+const { GUIProcess, TimeCalculation } = require('./Process');
 
 // class InputProcess {
 //     constructor(processId, arrivalTime, burstTime, priority, quantum) {
@@ -20,175 +19,158 @@ const {InputProcess,GUIProcess,TimeCalculation} = require("./Process")
 //     }
 // }
 
-class RoundRobin extends EventEmitter{
-    #QProcesses;        // running Q
-    #QuntumTime;        // QuntumTime to do context switching
-    #RefernceTime;      // Time taken to serve all the processes
-    #avgTurnAround;     // Average Turn Around Time
-    #avgWaitingTime;    // Average Waiting Time 
-    #totalProceses;     // Total number of procesess that will be served
-    #segments;          // keep track all segments that will be sent to the GUI
-    #QnewProcesses;
-    constructor(inputProcesses,QuntumTime){
-        super();
-        // sort by the arrival Time  
-        inputProcesses.sort((a,b) => a.arrivalTime - b.arrivalTime);
+class RoundRobin extends EventEmitter {
+	#QProcesses; // running Q
+	#QuantumTime; // QuantumTime to do context switching
+	#ReferenceTime; // Time taken to serve all the processes
+	#avgTurnAround; // Average Turn Around Time
+	#avgWaitingTime; // Average Waiting Time
+	#totalProcesses; // Total number of processes that will be served
+	#segments; // keep track all segments that will be sent to the GUI
+	#QnewProcesses;
 
-        this.#QnewProcesses = inputProcesses? inputProcesses.slice(1):[];
-        this.#QProcesses = inputProcesses? [inputProcesses[0]]:[];
-        
-        this.#QuntumTime = QuntumTime;
-        
-        this.#avgTurnAround = 0;
-        this.#avgWaitingTime = 0;
-        this.#totalProceses = inputProcesses?1:0;
+	constructor(inputProcesses, QuantumTime) {
+		super();
+		// sort by the arrival Time
+		inputProcesses.sort((a, b) => a.arrivalTime - b.arrivalTime);
 
-        
-        this.#RefernceTime = this.#QProcesses[0].arrivalTime; // set the reference time with the lowest arrival Time of the processes
+		this.#QnewProcesses = inputProcesses ? inputProcesses.slice(1) : [];
+		this.#QProcesses = inputProcesses ? [inputProcesses[0]] : [];
 
-        this.#segments  = [];        
+		this.#QuantumTime = QuantumTime;
+
+		this.#avgTurnAround = 0;
+		this.#avgWaitingTime = 0;
+		this.#totalProcesses = inputProcesses ? 1 : 0;
+
+		this.#ReferenceTime = this.#QProcesses[0].arrivalTime; // set the reference time with the lowest arrival Time of the processes
+
+		this.#segments = [];
+	}
+
+	async Run(drawAll = false) {
+		let sumTurnAround = 0; // acculate the turn around time for all served processes
+		let sumWaitingTime = 0; // acculate the waiting time for all served processes
+		let Live = true; // check if the algorithm is already running or terminated!
+
+		const CheckNewComingProcesses = setInterval(() => {
+			// check every one second for the new coming processes to push
+			// them @ the correct arrival time
+			// taking onto consideration the life status of the algorithm
+			// and processes that can come at the same arrival time!
+			if (!Live) clearInterval(CheckNewComingProcesses);
+
+			if (this.#QnewProcesses.length) {
+				for (let i = 0; i < this.#QnewProcesses.length; i++) {
+					if (this.#QnewProcesses[0].arrivalTime === this.#ReferenceTime) {
+						this.#QProcesses.push(this.#QnewProcesses[0]);
+						this.#QnewProcesses.shift();
+						this.#totalProcesses++;
+					} else break;
+				}
+			}
+		}, 1000);
+
+		while (this.#QProcesses.length) {
+			let currentRunningProcess = this.#QProcesses[0];
+			this.#QProcesses.shift();
+
+			let emittedProcess = new GUIProcess(
+				currentRunningProcess.processId,
+				this.#ReferenceTime,
+				this.#getEndTime(currentRunningProcess) + this.#ReferenceTime
+			);
+			if (!drawAll) await this.#serveProcess(currentRunningProcess, emittedProcess);
+			else {
+				await this.#serveProcess(currentRunningProcess, null);
+				this.#segments.push(emittedProcess);
+			}
+
+			if (currentRunningProcess.consumedTime < currentRunningProcess.burstTime) {
+				this.#QProcesses.push(currentRunningProcess);
+			} else {
+				sumTurnAround += this.#ReferenceTime - currentRunningProcess.arrivalTime;
+				sumWaitingTime +=
+					this.#ReferenceTime -
+					currentRunningProcess.arrivalTime -
+					currentRunningProcess.burstTime;
+			}
+
+			let counter = 0;
+			while (!this.#QProcesses.length && counter < 10) {
+				// wait 10 second until a new process come to the Q or terminate!
+				await this.#wait(1000);
+				this.#ReferenceTime++;
+				counter++;
+			}
+		}
+		Live = false;
+		this.#avgTurnAround = (sumTurnAround / this.#totalProcesses).toFixed(3);
+		this.#avgWaitingTime = (sumWaitingTime / this.#totalProcesses).toFixed(3);
+
+		if (drawAll) this.emit('drawAll', this.#segments);
+
+		let calculationObj = new TimeCalculation(this.#avgWaitingTime, this.#avgTurnAround);
+
+		this.emit('done', calculationObj);
+	}
+
+	#getEndTime(process) {
+		// input:   current running process
+		// output:  the remaining time for the process
+		let diff = process.burstTime - process.consumedTime;
+		let endTime = diff < this.#QuantumTime ? diff : this.#QuantumTime;
+		return endTime;
+	}
+
+	async #serveProcess(RunningProcess, emittedProcess) {
+		for (let i = 0; i < this.#QuantumTime; i++) {
+			if (RunningProcess.consumedTime >= RunningProcess.burstTime) {
+				break;
+			}
+
+			const waitTime = 1000;
+			await this.#wait(waitTime);
+			this.#ReferenceTime++;
+
+			if (emittedProcess) this.emit('draw', emittedProcess); // notify the GUI every one second
+
+			RunningProcess.consumedTime++;
+			if (RunningProcess.consumedTime % this.#QuantumTime === 0) {
+				break;
+			}
+		}
+	}
+
+	async #wait(ms) {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve();
+			}, ms);
+		});
+	}
+
+	#getCompletePercentage(Process) {
+		if (!Process.BurstTime) return 0;
+		else return parseInt(Math.floor((Process.consumedTime / Process.BurstTime) * 100));
+	}
+
+	appendToQueue(Process) {
+		this.#QnewProcesses.push(Process);
+		// this.#totalProceses++;
+	}
+
+	get avgTurnAround() {
+		return this.#avgTurnAround;
+	}
+
+	get avgWaitingTime() {
+		return this.#avgWaitingTime;
+	}
+
+    setQuantumTime(time){
+        this.#QuantumTime = time;
     }
-
-    async Run(drawAll = false){
-        let sumTurnAround = 0;          // acculate the turn around time for all served processes
-        let sumWaitingTime = 0;         // acculate the waiting time for all served processes
-        let Live = true;                // check if the algorithm is already running or terminated!
-
-        const ChecknNewComingProcesses = setInterval(()=>{
-            // check every one second for the new coming proccesses to push 
-            // them @ the correct arrival time
-            // taking onto consideration the life status of the algorithm 
-            // and procceses that can come at the same arrival time! 
-            if(!Live)
-                clearInterval(ChecknNewComingProcesses);
-            
-                if(this.#QnewProcesses.length){
-                for(let i = 0;i<this.#QnewProcesses.length;i++){
-                    if(this.#QnewProcesses[0].arrivalTime === this.#RefernceTime){
-                        this.#QProcesses.push(this.#QnewProcesses[0]);
-                        this.#QnewProcesses.shift();
-                        this.#totalProceses++;
-                    }
-                    else
-                        break;
-                }
-            }
-            
-        },1000);
-
-        while(this.#QProcesses.length){
-            let currentRunningProcess = this.#QProcesses[0];
-            this.#QProcesses.shift();
-
-            let emittedProcess = new GUIProcess(currentRunningProcess.processId,
-                                                this.#RefernceTime,
-                                                this.#getEndTime(currentRunningProcess)+this.#RefernceTime);
-            if(!drawAll)
-                await this.#serveProcess(currentRunningProcess,emittedProcess);
-            else{
-                await this.#serveProcess(currentRunningProcess,null); 
-                this.#segments.push(emittedProcess);
-            }   
-            
-            if(currentRunningProcess.consumedTime < currentRunningProcess.burstTime){
-                this.#QProcesses.push(currentRunningProcess);
-            }
-            else{
-                
-                sumTurnAround += this.#RefernceTime -currentRunningProcess.arrivalTime;
-                sumWaitingTime += this.#RefernceTime - currentRunningProcess.arrivalTime -currentRunningProcess.burstTime;
-
-            }
-
-
-            let counter = 0;
-            while (!this.#QProcesses.length && counter < 10){
-                // wait 10 second until a new process come to the Q or terminate!
-                await this.#wait(1000);
-                this.#RefernceTime++;
-                counter++;
-            }
-
-
-
-
-        }
-        Live = false;
-        console.log(this.#totalProceses);
-        // console.log(sumTurnAround,sumWaitingTime);
-        this.#avgTurnAround = (sumTurnAround/this.#totalProceses).toFixed(3);
-        this.#avgWaitingTime = (sumWaitingTime/this.#totalProceses).toFixed(3);
-        
-
-        if(drawAll)
-            this.emit("drawAll",this.#segments);
-
-    
-        let calculationObj = new TimeCalculation(this.#avgWaitingTime,this.#avgTurnAround);  
-
-
-        this.emit("Done",calculationObj);    
-
-    }
-
-
-    
-    #getEndTime(process){
-        // input:   current running process 
-        // output:  the remaining time for the process
-        let diff  = (process.burstTime-process.consumedTime)
-        let endTime =  diff  < this.#QuntumTime ? diff:this.#QuntumTime;
-        return endTime;
-    }
-    async #serveProcess(RunningProcess,emittedProcess){
-        
-        for(let i = 0;i<this.#QuntumTime;i++){
-            
-            if (RunningProcess.consumedTime >= RunningProcess.burstTime) {
-                break;
-            }
-
-            const waitTime = 1000; 
-            await this.#wait(waitTime);
-            this.#RefernceTime ++;
-            
-            if(emittedProcess)
-                this.emit('draw',emittedProcess);   // notify the GUI every one second
-                
-            RunningProcess.consumedTime++;
-            if (RunningProcess.consumedTime % this.#QuntumTime === 0) {
-                break;
-            }
-
-            
-        }
-    
-    }
-    async #wait(ms) {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve();
-            }, ms);
-        });
-    }
-
-    #getCompletePercentage(Process){
-        if (!Process.BurstTime)
-            return 0;
-        else    
-            return parseInt(Math.floor((Process.consumedTime/Process.BurstTime)*100));
-    }
-    pushProcess(Process){
-        this.#QnewProcesses.push(Process);
-        // this.#totalProceses++;
-    }
-
-    get avgTurnAround(){return this.#avgTurnAround;}
-    get avgWaitingTime(){return this.#avgWaitingTime;}
 }
 
-
-
-module.exports = {
-    RoundRobin,
-};
+module.exports = RoundRobin;
--- src/util/RoundTest.js
+++ src/util/RoundTest.js
@@ -1,63 +1,56 @@
-// import { RoundRobin } from "./RoundRobin";
-const {RoundRobin}  = require('./RoundRobin.js');
-const {InputProcess}  =require('./Process.js');
-    
+const RoundRobin = require('./RoundRobin.js');
+const { InputProcess } = require('./Process.js');
 
-function initializeProcesses(num_processes){
-        let processes = [];
-        const startTime = Date.now();
-        for (let index = 0; index < num_processes; index++) {
-            const proceses = new InputProcess(index,
-                                    Date.now()-startTime+index,
-                                    Math.floor(Math.random()*5+1),index);
-            
-            processes.push(proceses);
-        }
-        return processes;
-    }
+function initializeProcesses(num_processes) {
+	let processes = [];
+	const startTime = Date.now();
+	for (let index = 0; index < num_processes; index++) {
+		const process = new InputProcess(
+			index,
+			Date.now() - startTime + index,
+			Math.floor(Math.random() * 5 + 1),
+			index
+		);
 
-function printProcessesInfo(processes){
-        for(const process of processes){
-            console.log(process.toString());
-        }
-    }
+		processes.push(process);
+	}
+	return processes;
+}
 
+function printProcessesInfo(processes) {
+	for (const process of processes) {
+		console.log(process.toString());
+	}
+}
 
+// start Time of the algorithm
+let startTime = Date.now();
+let processes = initializeProcesses(4);
+printProcessesInfo(processes);
 
-    // start Time of the algorithm
-    let startTime = Date.now();
-    let proceses = initializeProcesses(4);
-    printProcessesInfo(proceses);
+let algo = new RoundRobin(processes, 3);
+algo.Run(true);
 
-    let algo= new RoundRobin(proceses,3);
-    algo.Run();
+let tester = [];
+algo.on('draw', (segment) => {
+	tester.push(segment);
+	console.log(segment);
+});
 
+algo.on('append', (process) => {
+	algo.appendToQueue(process);
+});
 
-        
-    let tester = [];
-    algo.on('draw', (segment) => {
-        tester.push(segment);
-        console.log(segment);
-    }); 
+algo.on('drawAll', (arr) => {
+	console.log(arr);
+});
 
-    algo.on('append', (process) => {
-        algo.pushProcess(process);
-    })
+algo.on('done', (Time) => {
+	console.log(Time);
+});
 
-    algo.on('drawAll', (arr) => {
-            console.log(arr);
-    })
-
-
-    algo.on("Done",(Time)=>{
-        console.log(Time);
-    })
-
-    setTimeout(()=>{
-        let processNew = new InputProcess(4,10,5,0,0);
-        algo.pushProcess(processNew);
-        console.log(`New ${processNew.toString()}`);
-    },2000);
-    // setInterval(()=>{
-        
-    // })
+setTimeout(() => {
+	let processNew = new InputProcess(4, 10, 5, 0, 0);
+	algo.appendToQueue(processNew);
+	console.log(`New ${processNew.toString()}`);
+}, 2000);
